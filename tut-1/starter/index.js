b7a6c811916d17dad5d0edc70a2c3e3272070113
Andrew Blair <blair.d.andrew@gmail.com>
2020-01-17 17:25:18 +0000

    Asynchronous and  Synchronous example done

--- tut-1/starter/index.js
+++ tut-1/starter/index.js
@@ -1,9 +1,27 @@
 const fs = require('fs'); // fs = filesystem
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8'); //no uft-8 willl return a buffer
-console.log(textIn);
 
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// blocking synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8'); //no uft-8 willl return a buffer
+// console.log(textIn);
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('file written');
 
-fs.writeFileSync('./txt/output.txt', textOut);
-console.log('file written')
+// non blocking asynchronous way
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+    if (err) return console.log('error :(');
+
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        console.log(data2);
+        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+            console.log(data3);
+
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`,'utf-8', err => {
+            console.log('file has been written');
+            })
+        });
+    });
+});
+
+console.log('will read file');
