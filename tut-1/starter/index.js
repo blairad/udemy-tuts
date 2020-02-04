82c3ab7b17ff9eb3667bcd77e38d2af527a079f2
Andrew Blair <blair.d.andrew@gmail.com>
2020-02-04 22:57:16 +0000

    deleting things and having to find where they went!

--- tut-1/starter/index.js
+++ tut-1/starter/index.js
@@ -33,6 +33,25 @@ const url = require('url');
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 //SERVER
 
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    //product from arg above and .Name taking what is needed from json
+    //using /***/g as there are multiple of that name and backtics will jsut get the first placeholder
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if (!product.organic)
+        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+}
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObject = JSON.parse(data);
@@ -42,20 +61,27 @@ const server = http.createServer((req, res) => {
 
     // Overview Page
     if (pathName === '/' || pathName === '/overview') {
-        res.end('this is the OVERVIEW');
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        });
 
-    // Product Page
+        const cardsHtml = dataObject.map(element => replaceTemplate(tempCard, element)).join('');
+        // element is what holds the data from the json file
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
+        res.end(output);
+
+        // Product Page
     } else if (pathName === '/product') {
         res.end('this is the PRODUCT');
 
-    // API 
+        // API 
     } else if (pathName === '/api') {
         res.writeHead(200, {
             'Content-type': 'application/json'
-        })
+        });
         res.end(data); // data here is taken from the data var at the top as it's inthe callback function and has access to the code higher up
 
-    // Not Found
+        // Not Found
     } else {
         res.writeHead(404, {
             // http header is a piece of into about the response we are sending back and is sent in an object. they should be sent before the response content
